a482c1a2380c09b101e00a8adaf19215f1f99185
gp559 <prasadgaurav559@gmail.com>
2020-10-15 19:06:57 +0530

    chat folder added

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA6NEe_gU_VW2_kYlRYVbkB0t2svEx7HWI",
+    authDomain: "grapido-chat.firebaseapp.com",
+    databaseURL: "https://grapido-chat.firebaseio.com",
+    projectId: "grapido-chat",
+    storageBucket: "grapido-chat.appspot.com",
+    messagingSenderId: "46917096169",
+    appId: "1:46917096169:web:1099b153de11d0e3955678",
+    measurementId: "G-RFZ6BRY698"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
